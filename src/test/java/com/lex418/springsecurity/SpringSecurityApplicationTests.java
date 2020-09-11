4d0896aa740583e005d8fecad82e29d5b01e6570
lex418 <rakesh418@gmail.com>
2020-09-11 19:37:35 +0530

    First Commit from local

---
+++ src/test/java/com/lex418/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lex418.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
