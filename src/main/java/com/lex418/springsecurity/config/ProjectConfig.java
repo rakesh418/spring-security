efc48950a3a289cf2d35800c32a17d7485245e2e
lex418 <rakesh418@gmail.com>
2020-09-18 00:47:54 +0530

    UserDetailsmanager implementation

--- src/main/java/com/lex418/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/lex418/springsecurity/config/ProjectConfig.java
@@ -1,25 +1,45 @@
 package com.lex418.springsecurity.config;
 
 import com.lex418.springsecurity.services.JPAUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource datasource;
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        return new JPAUserDetailsService();
+    public JdbcUserDetailsManager userDetailsService(){
+        return new JdbcUserDetailsManager(datasource);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http.httpBasic();
+        http.csrf().disable();
+        http.authorizeRequests().mvcMatchers("/user").permitAll().anyRequest().authenticated();
     }
 }
